dbd2eb56bebb6f8a5ae3d13ac5927af1f97733f1
Mikey <125025968+Mikey-debug@users.noreply.github.com>
2023-05-30 22:15:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,23 @@
+import asyncio
 from app import app
 
-if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+async def run_flask_app():
+    app.run(debug=True, host='0.0.0.0')
+
+async def run_async_app():
+    # Your async app logic goes here
+    # Replace this with your actual async app code
+
+if name == 'main':
+    loop = asyncio.get_event_loop()
+    
+    flask_task = loop.create_task(run_flask_app())
+    async_task = loop.create_task(run_async_app())
+    
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        loop.run_until_complete(asyncio.gather(flask_task, async_task))
+        loop.close()
