b083cb2ac02c8bb81a6ef4f5ee7b08e23dd69c6e
Mikey <125025968+Mikey-debug@users.noreply.github.com>
2023-06-09 19:39:25 +0530

    Update download.py

--- scrappers/pahe/download.py
+++ scrappers/pahe/download.py
@@ -13,7 +13,7 @@ def pahe_download(idx):
     chrome_options.add_argument("--disable-gpu")
 
 #     uc.install()
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome("/usr/local/bin/chromedriver", options=chrome_options)
 #     driver = uc.Chrome()
 
 #     driver.get(download_url[0])
