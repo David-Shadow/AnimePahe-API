13fca859d1c3488f4d140a6309c1518a1624d40c
Mikey <125025968+Mikey-debug@users.noreply.github.com>
2023-06-09 19:08:06 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from app import app
 from scrappers.pahe.download import pahe_download
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+#     app.run(host='0.0.0.0', debug=True)
     print(pahe_download('f9825cf8-ac20-a4bf-9530-951fe7e38032$642492e502f5afba06ce1c206bc5be18cb28b685796f9712e955f0fe38867ac7'))
